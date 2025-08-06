0087179f8e12c6105ddb790209f56aedc49f422f
marcossouza1232 <100810347+marcossouza1232@users.noreply.github.com>
2025-08-05 21:24:58 -0300

    Update fileorganizer.py

--- fileorganizer.py
+++ fileorganizer.py
@@ -14,33 +14,28 @@ scandir_result = scandir(directory)
 for direntry in scandir_result:
     if direntry.is_file():
         name = direntry.name
-        if not ( argv[0][2:] == name or argv[0] == name ): # This line filters out the script that initiated the execuction.
+        if not ( ( argv[0][2:] == directory + "/" + name)  or ( argv[0] == directory + "/" + name ) ): # This line filters out the script that initiated the execuction.
             files.append(name)
 
-extensions = {'scripts': ['.py', '.sh', '.c'], 'docs': ['.txt', '.pdf', '.docx'], 'images' : ['.jpg', '.jpeg', '.png']}
+extensions = {'scripts': ['.py', '.sh', '.c'], 'docs': ['.txt', '.pdf', '.docx'], 'images' : ['.jpg', '.jpeg', '.png'], 'others': []}
 
-organized_files = {'scripts': [], 'docs': [], 'images': [], 'others': []}
+for file_type in extensions:
+    try:
+        mkdir(directory + "/" + file_type)
+    except FileExistsError:
+        print("Directory '{0}' already exists - skipping creation".format(file_type))
 
 for f in files:
     achou = False
     for file_type in extensions:
         for extension in extensions[file_type]:
             if f.endswith(extension):
-                organized_files[file_type].append(f)
+                rename(directory + "/" + f, directory + "/" + file_type + "/" + f)
                 achou = True
                 break
+        if achou:
+            break
     if not achou:
-        organized_files['others'].append(f)
-
-for file_type in organized_files:
-    try:
-        mkdir(directory + "/" + file_type)
-    except FileExistsError:
-        print("Directory '{0}' already exists - skipping creation".format(file_type))
-
-for file_type in organized_files:
-    for f in organized_files[file_type]:
-        rename(directory + "/" + f, directory + "/" + file_type + "/" + f)
-
+        rename(directory + "/" + f,directory + "/" + "others" + "/" + f)
 
 print("The directory has been successfuly organized")
